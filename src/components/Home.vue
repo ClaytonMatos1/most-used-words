30384b2b18553f65b07de642d1e629344550cf06
cmatos <cmatos@uolinc.com>
2021-11-04 19:39:19 -0300

    [master] initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,77 @@
+<template>
+    <v-container fluid>
+        <v-form>
+            <v-file-input
+                label="Selecione as legendas"
+                prepend-icon="mdi-message-text"
+                append-outer-icon="mdi-send"
+                outlined
+                multiple
+                chips
+                v-model="files"
+                @click:append-outer="processSubtitles"
+            />
+        </v-form>
+        <div class="pills">
+            <pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+        </div>
+    </v-container>
+</template>
+
+<script>
+import Pill from './Pill.vue';
+export default {
+    name: "Home",
+    components: {
+        Pill,
+    },
+    data() {
+        return {
+            files: [],
+            groupedWords: [
+                {
+                    name: "are",
+                    amount: 800,
+                },
+                {
+                    name: "the",
+                    amount: 700,
+                },
+                {
+                    name: "best",
+                    amount: 600,
+                },
+                {
+                    name: "in",
+                    amount: 500,
+                },
+                {
+                    name: "world",
+                    amount: 300,
+                },
+                {
+                    name: "and",
+                    amount: 200,
+                },
+                {
+                    name: "you",
+                    amount: 100,
+                },
+            ],
+        };
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files);
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="pill">
+        <span class="name">{{ name }}</span>
+        <span class="amount">{{ amount }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "pill",
+    props: {
+        name: {
+            type: String,
+            default: '',
+        },
+        amount: {
+            type: Number,
+            default: 0,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.pill {
+    display: flex;
+    align-items: center;
+    margin: 10px;
+    height: 55px;
+    border-radius: 30px;
+    font-size: 1.3rem;
+    font-weight: bold;
+    background-color: #dc143c;
+    color: #ffffff;
+
+    .name {
+        margin-left: 15px;
+    }
+
+    .amount {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 10px;
+        padding: 5px;
+        width: 35px;
+        height: 35px;
+        border-radius: 18px;
+        font-size: 14px;
+        background-color: #ffffff;
+        color: #dc143c;
+    }
+}
+</style>
